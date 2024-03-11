6535ada305f792e3256f29ffff57bd80c3eda045
kuang-yu-wang <211410146@o365.tku.edu.tw>
2024-03-11 18:56:44 +0800

    Fix P10 Poly can not sum correctly problem

--- 240304/c/p10.c
+++ 240304/c/p10.c
@@ -5,29 +5,25 @@ void printPoly(int poly[]);
 void sumPoly(int poly1[ITEMS], int poly2[ITEMS]);
 
 int main(int argc, char const *argv[]) {
-  int polyA[ITEMS] = {4, 3, 7, 0, 6, 2};
-  int polyB[ITEMS] = {4, 1, 5, 2, 0, 9};
-  printf("多項式A：");
-  printPoly(polyA);
-  printf("多項式B：");
-  printPoly(polyB);
-  printf("多項式A + 多項式B：");
-  sumPoly(polyA, polyB);
+  int PolyA[ITEMS]={4,3,7,0,6,2}, PolyB[ITEMS]={4,1,5,2,0,9};
+printf("多項式A=>");PrintPoly(PolyA, ITEMS);
+printf("多項式B=> "); PrintPoly(PolyB, ITEMS);
+printf ("A+B => "); PolySum(PolyA,PolyB);
   return 0;
 }
 
-void printPoly(int poly[]) {
-  int i, maxExp;
-  maxExp = poly[0];
-  for (int i = 1; i <= poly[0] + 1; i++) {
-    maxExp--;
-    if (poly[i] == 0)
+void printPoly(int Poly[]) {
+  int i,MaxExp;
+  MaxExp= Poly[0];
+  for (i=1;i<=Poly[0]+1;i++) {
+    MaxExp--;
+    if (Poly[i] == 0)
       break;
-    if ((maxExp + 1) == 0)
-      printf(" %d", poly[i]);
+    if ((MaxExp + 1) == 0)
+      printf(" %d", Poly[i]);
     else
-      printf(" %dX^%d", poly[i], maxExp + 1);
-    if (maxExp >= 0)
+      printf(" %dX^%d", Poly[i], MaxExp + 1);
+    if (MaxExp >= 0)
       printf("+");
   }
   printf("\n");
